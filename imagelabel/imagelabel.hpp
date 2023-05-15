4c520f486b84cbf1bc0efa26cae92521b31749b3
Chenjingzhi-cjb <1286576927@qq.com>
2023-05-15 16:00:53 +0800

    Optimized the display of value.

--- imagelabel/imagelabel.hpp
+++ imagelabel/imagelabel.hpp
@@ -41,17 +41,50 @@ private:
                 color = m_qpixmap.toImage().pixelColor(x, y);
             }
 
-            std::string result = " (" + std::to_string(x) + ", " + std::to_string(y) + ") ";
-            result += "->";
+            std::string result = "XY ";
+            result += "(" + std::to_string(x) + ", " + std::to_string(y) + ") ";
+            result += "-> ";
 
-            if (image_type == ImageType::RGB || image_type == ImageType::GRAY) {
-                result += " (" + std::to_string(color.red()) + ", " + std::to_string(color.green()) + ", " + std::to_string(color.blue()) + ") ";
+            if (image_type == ImageType::RGB) {
+                result += "RGB ";
+                result += "(" + std::to_string(color.red()) + ", " + std::to_string(color.green()) + ", " + std::to_string(color.blue()) + ") ";
+                result += "#" + intToHexStr(color.red()) + intToHexStr(color.green()) + intToHexStr(color.blue());
+            } else if (image_type == ImageType::GRAY) {
+                result += "Gray ";
+                result += std::to_string(color.green());
             } else if (image_type == ImageType::HSI) {
-                result += " (" + std::to_string(color.blue()) + ", " + std::to_string(color.green()) + ", " + std::to_string(color.red()) + ") ";
+                result += "HSI ";
+                result += "(" + std::to_string(color.blue()) + ", " + std::to_string(color.green()) + ", " + std::to_string(color.red()) + ") ";
+                result += "#" + intToHexStr(color.blue()) + intToHexStr(color.green()) + intToHexStr(color.red());
             }
 
             emit mouseSignal(QString::fromStdString(result));
         }
+    }
+
+    std::string intToHexStr(int num) {
+        std::string result = "";
+
+        int quo;
+        while ((quo = num / 16) != 0) {
+            if (quo < 10) {
+                result += ('0' + quo);
+            } else {  // quo >= 10
+                quo -= 10;
+                result += ('A' + quo);
+            }
+
+            num %= 16;
+        }
+
+        if (num < 10) {
+            result += ('0' + num);
+        } else {  // quo >= 10
+            num -= 10;
+            result += ('A' + num);
+        }
+
+        return result;
     }
 
 signals:
